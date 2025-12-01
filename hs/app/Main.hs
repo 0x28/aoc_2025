aaf339b8f9e523a1a9d1ba2db439ce18810ad4d1
0x28 <12886563+0x28@users.noreply.github.com>
2025-12-01 21:52:17 +0100

    Add haskell solution

---
+++ hs/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Day01 qualified (day01)
+
+main :: IO ()
+main = Day01.day01
---
+++ hs/test/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import Day01
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented."
